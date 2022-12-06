b162c8b1de0b982d28fa03feffe5a6bdd2968977
Svilen Ivanov <svilen.ivanov@gmail.com>
2022-12-06 07:12:25 +0200

    Day 6

--- src/aoc2020/day06/Day06.kt
+++ src/aoc2020/day06/Day06.kt
@@ -3,15 +3,35 @@ package aoc2020.day06
 import readInput
 
 fun main() {
-    fun part1(input: List<String>) {
+    fun findMarker(line: String, uniqueCount: Int): Int {
+        val deque = ArrayDeque<Char>()
+        for ((index, char) in line.withIndex()) {
+            deque.addLast(char)
+            if (deque.size < uniqueCount) continue
+            if (deque.toSet().size == uniqueCount) {
+                val pos = index + 1
+                println(pos)
+                return pos
+            } else {
+                deque.removeFirst()
+            }
+        }
+        error("")
+    }
 
+    fun part1(input: List<String>) {
+        for (line in input) {
+            check(findMarker(line, 4) == 1779)
+        }
     }
 
     fun part2(input: List<String>) {
-
+        for (line in input) {
+            check(findMarker(line, 14) == 2635)
+        }
     }
 
-    val testInput = readInput("day06/day06s")
+    val testInput = readInput("day06/day06")
     part1(testInput)
     part2(testInput)
 }
