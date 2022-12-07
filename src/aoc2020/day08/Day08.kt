101dda1c35358452dcf1d8b2110b400ec5cefb92
Svilen Ivanov <svilen.ivanov@gmail.com>
2022-12-07 08:05:27 +0200

    Added templates for the next days

---
+++ src/aoc2020/day08/Day08.kt
@@ -0,0 +1,15 @@
+package aoc2020.day08
+
+import readInput
+
+fun main() {
+    fun part1(input: List<String>) {
+    }
+
+    fun part2(input: List<String>) {
+    }
+
+    val testInput = readInput("day08/day08")
+    part1(testInput)
+    part2(testInput)
+}
---
+++ src/aoc2020/day09/Day09.kt
@@ -0,0 +1,15 @@
+package aoc2020.day09
+
+import readInput
+
+fun main() {
+    fun part1(input: List<String>) {
+    }
+
+    fun part2(input: List<String>) {
+    }
+
+    val testInput = readInput("day09/day09")
+    part1(testInput)
+    part2(testInput)
+}
