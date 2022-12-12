174848899212fa47db979241d08fdc1ba58a3702
Svilen Ivanov <svilen.ivanov@gmail.com>
2022-12-12 10:06:17 +0200

    Prepare day 14

---
+++ src/aoc2020/day14/Day14.kt
@@ -0,0 +1,16 @@
+package aoc2020.day14
+
+import readInput
+
+fun main() {
+
+    fun part1(input: List<String>) {
+    }
+
+    fun part2(input: List<String>) {
+    }
+
+    val testInput = readInput("day14/day14s")
+    part1(testInput)
+    part2(testInput)
+}
